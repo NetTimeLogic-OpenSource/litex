c3b24bedc563a9b10083743d4a24f0bbbf41b8c9
Kevin Schaerer <kevin.schaerer@nettimelogic.com>
2025-08-15 17:29:47 +0200

    adapt to comply with NTL Hive-S workflow
        * removed unused features from BIOS to reduce binary size
        * improved booting from SPI Flash by loading data from flash
          to RAM by looking up a corresponding mapping table
        * now all sources are copied over to the build directory
        * added generation of IP of finished SoC to easily use it in
          another project (Vivado only)

--- litex/tools/litex_json2dts_linux.py
+++ litex/tools/litex_json2dts_linux.py
@@ -655,7 +655,7 @@ def generate_dts(d, initrd_start=None, initrd_size=None, initrd=None, root_devic
 
     # SPI Flash ------------------------------------------------------------------------------------
 
-    if "spiflash" in d["csr_bases"]:
+    if "spiflash_core" in d["csr_bases"]:
         aliases["spiflash"] = "litespiflash"
         dts += """
             litespiflash: spiflash@{spiflash_csr_base:x} {{
@@ -668,7 +668,7 @@ def generate_dts(d, initrd_start=None, initrd_size=None, initrd=None, root_devic
                     reg = <0x0 0x{spiflash_size:x}>;
                 }};
             }};
-""".format(spiflash_csr_base=d["csr_bases"]["spiflash"], spiflash_size=d["memories"]["spiflash"]["size"])
+""".format(spiflash_csr_base=d["csr_bases"]["spiflash_core"], spiflash_size=d["memories"]["spiflash"]["size"])
 
     # SPI-SDCard -----------------------------------------------------------------------------------
 
